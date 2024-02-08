310b44bbfc57bd8177cb1dba6014ad4625b6f963
Saif Aldin Sunjaq <saifmarwan774@gmail.com>
2024-02-08 15:02:18 +0400

    Hamburger button fixed.

--- Script.js
+++ Script.js
@@ -1,7 +1,31 @@
-const navbarElements = document
-  .getElementsByClassName("navbar")
-  .getElementsByTagName("li");
+const navbarElements = document.querySelectorAll(".jsLi");
+
+console.log(navbarElements);
+
+let hambHidden = true;
 
 function hambShow() {
-  navbarElements.style.display = "block";
+  if(hambHidden == true) {
+    for(let i = 0; i < navbarElements.length; ++i) {
+      navbarElements[i].style.display = "block";
+    }
+    hambHidden = false;
+  } else {
+    for(let i = 0; i < navbarElements.length; ++i) {
+      navbarElements[i].style.display = "none";
+    }
+    hambHidden = true;
+  }
 }
+
+// window.addEventListener("resize", () => {
+//   if(screen.width >= 1200) {
+//     for(let i = 0; i < navbarElements.length; ++i) {
+//       navbarElements[i].style.display = "block";
+//     }
+//   } else {
+//     for(let i = 0; i < navbarElements.length; ++i) {
+//       navbarElements[i].style.display = "none";
+//     }
+//   }
+// })
